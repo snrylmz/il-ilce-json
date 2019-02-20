51f9091e03bcd070f07ef592322867b54a3f55ca
Şener Yılmaz <senerov@senerov.com>
2019-02-20 13:12:41 +0300

    İzmir, Kars verileri eklendi

--- index.js
+++ index.js
@@ -12,20 +12,23 @@ $('#data-table').DataTable({
           {"data": "nufus_yuzdesi_genel"},
           {"data": "ilceler.length"}
      ],
+     columnDefs: [
+          {type: 'turkish', targets: '_all'}
+      ],
      "language": {
            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
    },
-   "lengthMenu": [[25, 50, -1], [25, 50, "All"]],
-   "initComplete": function(){  // dataTable yüklemesi tamamlandıktan sonra tr satırlarının içini okur ve renklendirir.
-         $("tr:contains('Karadeniz')" ).css( "background-color", "#c8e6c9" );
-         $("tr:contains('Marmara')" ).css( "background-color", "#f5f5f5" );
-         $("tr:contains('Ege')" ).css( "background-color", "#b2ebf2" );
-         $("tr:contains('İç Anadolu')" ).css( "background-color", "#fff9c4" );
-         $("tr:contains('Akdeniz')" ).css( "background-color", "#b3e5fc" );
-         $("tr:contains('Doğu Anadolu')" ).css( "background-color", "#ffe0b2" );
-         $("tr:contains('Güneydoğu Anadolu')" ).css( "background-color", "#d7ccc8" );
-   },
-});
+     "lengthMenu": [[-1], [-1]],
+     "initComplete": function(){  // dataTable yüklemesi tamamlandıktan sonra tr satırlarının içini okur ve renklendirir.
+           $("tr:contains('Karadeniz')" ).css( "background-color", "#c8e6c9" );
+           $("tr:contains('Marmara')" ).css( "background-color", "#f5f5f5" );
+           $("tr:contains('Ege')" ).css( "background-color", "#b2ebf2" );
+           $("tr:contains('İç Anadolu')" ).css( "background-color", "#fff9c4" );
+           $("tr:contains('Akdeniz')" ).css( "background-color", "#b3e5fc" );
+           $("tr:contains('Doğu Anadolu')" ).css( "background-color", "#ffe0b2" );
+           $("tr:contains('Güneydoğu Anadolu')" ).css( "background-color", "#d7ccc8" );
+     },
+  });
 
 var table = $('#data-table').DataTable();
 $('#data-table tbody').on('click', 'tr', function () {
